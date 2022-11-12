6263815a59f6b0c6f42f5db38b45b0a3c1d17f40
abby-moana <abbymoana1@gmail.com>
2022-11-13 05:59:11 +0800

    option 2

--- Dictionary.py
+++ Dictionary.py
@@ -6,22 +6,23 @@ print("""      1 -----> Add an item
 
 personal_data = {}
 
-option = int(input("What do you want to do? (1-3): "))
+while True:
+    option = int(input("What do you want to do? (1-3): "))
+    if option == 1:
+        fullname = input("Full Name: ").title()
+        age = input("Age: ")
+        gender = input("Gender: ").title()
+        address = input("Address: ").title()
+        number = input("Phone Number: ")
+        print("Saved!")
 
-if option == 1:
-    fullname = input("Full Name: ").title()
-    age = input("Age: ")
-    gender = input("Gender: ").title()
-    address = input("Address: ").title()
-    number = input("Phone Number: ")
-    print("Saved!")
-
-    personal_data[fullname] = {}
-    personal_data[fullname]["Full Name"] = fullname
-    personal_data[fullname]["Age"] = age
-    personal_data[fullname]["Gender"] = gender
-    personal_data[fullname]["Address"] = address
-    personal_data[fullname]["Phone Number"] = number
-
-    print(personal_data)
+        personal_data[fullname] = {}
+        personal_data[fullname]["Full Name"] = fullname
+        personal_data[fullname]["Age"] = age
+        personal_data[fullname]["Gender"] = gender
+        personal_data[fullname]["Address"] = address
+        personal_data[fullname]["Phone Number"] = number
+    elif option == 2:
+        search = personal_data.get(input("Full name: ").title())
+        print(search)
 
