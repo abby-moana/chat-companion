ad78fae8f18d4797d10bcf3500c65c146c0cc013
abby-moana <abbymoana1@gmail.com>
2022-11-13 02:15:02 +0800

    option 1 for asking personal data

--- Dictionary.py
+++ Dictionary.py
@@ -1,5 +1,15 @@
-print("---------- MENU --------------")
+print("\n---------- MENU --------------")
 print("""      1 -----> Add an item
       2 -----> Search
       3 ------> Exit (y/n)
 ------------------------------""")
+
+option = int(input("What do you want to do? (1-3): "))
+if option == 1:
+    full_name = input("Full name: ")
+    gender = input("Gender: ")
+    age = input("Age: ")
+    bday = input("Birthday: ")
+    address = input("Address: ")
+    number = input("Phone Number: ")
+
