0fb0ad223685ee0743988a7f7364344e8814f687
abby-moana <abbymoana1@gmail.com>
2022-11-13 06:48:34 +0800

    added temperature in personal information

--- Dictionary.py
+++ Dictionary.py
@@ -1,21 +1,22 @@
-print("\n---------- MENU --------------")
+print("\n---------- CONTACT TRACING MENU --------------")
 print("""      1 -----> Add an item
       2 -----> Search
       3 ------> Exit (y/n)
-------------------------------""")
+----------------------------------------------""")
 
 personal_data = {}
 
 while True:
     option = int(input("\nWhat do you want to do? (1-3): "))
     if option == 1:
-        fullname = input("Full Name: ").title()
+        fullname = input("\nFull Name: ").title()
         age = input("Age: ")
         gender = input("Gender: ").title()
         address = input("Address: ").title()
         number = input("Phone Number: ")
         email = input("Email: ")
         time = input("Time of visit: ")
+        temperature = input("Temperature: ")
         print("Saved!")
 
         personal_data[fullname] = {}
@@ -26,18 +27,19 @@ while True:
         personal_data[fullname]["Phone Number"] = number
         personal_data[fullname]["Email"] = email
         personal_data[fullname]["Time"] = time
+        personal_data[fullname]["Temperature"] = temperature
 
     elif option == 2:
         search = input("Full name: ").title()
         for key, value in personal_data[search].items():
             print(key, ":", value)
     elif option == 3:
-        Exit = input("Exit? (y/n): ")
+        Exit = input("\nExit? (y/n): ")
         if Exit == "n":
             continue
         else:
             print("\n----------Thank you for using the program----------")
             break
     else:
-        print("\nInvalid input. \nExiting the program...")
+        print("\nInvalid input! \nExiting the program...")
         break
