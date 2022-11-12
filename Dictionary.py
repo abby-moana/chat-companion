cbb1737337237a4d1165c218e9bc7154611d6b01
abby-moana <abbymoana1@gmail.com>
2022-11-13 05:13:25 +0800

    storing info in dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -4,12 +4,15 @@ print("""      1 -----> Add an item
       3 ------> Exit (y/n)
 ------------------------------""")
 
-option = int(input("What do you want to do? (1-3): "))
-if option == 1:
-    full_name = input("Full name: ")
-    gender = input("Gender: ")
-    age = input("Age: ")
-    bday = input("Birthday: ")
-    address = input("Address: ")
-    number = input("Phone Number: ")
+personal_data = {}
 
+option = int(input("What do you want to do? (1-3): "))
+
+
+if option == 1:
+    fullname = input("Full Name: ").title()
+    age = input("Age: ")
+    gender = input("Gender: ").title()
+    address = input("Address: ").title()
+    number = input("Phone Number: ")
+    print("Saved!")
