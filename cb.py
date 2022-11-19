d25ee985a6c653198d1d755892f68944a1f7b9b2
abby-moana <abbymoana1@gmail.com>
2022-11-20 03:48:56 +0800

    while statement

--- cb.py
+++ cb.py
@@ -7,4 +7,11 @@ while True:
     user = input("\nYou: ")
     if user == "exit":
         break
+    elif user == "hi" or "hello" or "hey":
+        print("What is your name?")
+        name = input("You: ").title()
+        print("How are you, ", name, "?")
+        user = input("You: ")
+        print("CC is happy that you are doing well, ", name, ".")
+        print("\nTeach me anything. y/n")
 
