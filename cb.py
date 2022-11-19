923a4b2d45757e4936e19d8ff67c923a90afa7ed
abby-moana <abbymoana1@gmail.com>
2022-11-20 04:55:46 +0800

    changing variable name

--- cb.py
+++ cb.py
@@ -11,7 +11,7 @@ elif user == "hi" or "hello" or "hey":
     print(name, ", teach me anything. y/n")
 
 while True:
-    answer = input("You: ")
+    answer = input("Answer: ")
     if answer == "y":
         while True:
             print("\nType 'Done' if already satisfied in teaching CC.")
@@ -20,7 +20,16 @@ while True:
                 break
             cc = input("CC will say: ").title()
             dictionary[user] = cc
-
     else:
         print("Too bad. Maybe you can teach me next time :D")
         break
+
+while True:
+    user = input("You: ").title()
+    if user == "Exit":
+        break
+    if user in dictionary:
+        print("CC: ", dictionary[user])
+    else:
+        print("I'm sorry I haven't learned that one yet.")
+        continue
