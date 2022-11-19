5c0d20a1a6bd7ec767d8eb7aef77f0a486ce2f62
abby-moana <abbymoana1@gmail.com>
2022-11-20 04:45:04 +0800

    teaching CC

--- cb.py
+++ cb.py
@@ -2,15 +2,25 @@ dictionary = {}
 
 print("\nHello! I'm CC your chat companion.")
 
+user = input("\nYou: ").title()
+if user == "exit":
+    exit()
+elif user == "hi" or "hello" or "hey":
+    print("Hello! What is your name?")
+    name = input("You: ").title()
+    print(name, ", teach me anything. y/n")
 
 while True:
-    user = input("\nYou: ")
-    if user == "exit":
+    answer = input("You: ")
+    if answer == "y":
+        while True:
+            print("\nType 'Done' if already satisfied in teaching CC.")
+            user = input("If I say: ").title()
+            if user == "Done":
+                break
+            cc = input("CC will say: ").title()
+            dictionary[user] = cc
+
+    else:
+        print("Too bad. Maybe you can teach me next time :D")
         break
-    elif user == "hi" or "hello" or "hey":
-        print("What is your name?")
-        name = input("You: ").title()
-        print("How are you, ", name, "?")
-        user = input("You: ")
-        print("CC is happy that you are doing well, ", name, ".")
-        print("\nTeach me anything. y/n")
